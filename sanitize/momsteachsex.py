2e1c2e5a8e4c03efec3b28344a8b67440471e7d0
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-20 17:20:42 +0530

    Create momsteachsex.py

---
+++ sanitize/momsteachsex.py
@@ -0,0 +1,11 @@
+import clipboard as c
+
+
+text = c.paste()
+text = text.strip()
+title = text.split(".")[0]
+date = text.split(".")[1:4]
+mid_text = text.split(".")[4:-3]
+final = f"{title} - {' '.join(mid_text)} - {'.'.join(date)}"
+c.copy(final)
+print(final)
